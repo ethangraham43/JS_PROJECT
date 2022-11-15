976fc655b4bd705deceb86d7dea67a38e47b2ae3
Ethan Graham <ethangraham43@gmail.com>
2022-11-15 14:16:27 -0500

    cleaned up map.js and added team.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 // // import Example from "./scripts/example";
 import Map from "./scripts/map.js"
+import Team from "./scripts/team.js"
 
 document.addEventListener("DOMContentLoaded", () => {
     // const main = document.getElementById("main");
-    // new map(main);
-    console.log("hello")
+    // new Team(main);
+    // main.on("mouseover", function(d) {
+    //     main.select(main.ele).classed("selected", true)
+    // })
+    // main.on("mouseout", function(d) {
+    //     main.select(main.ele).classed("selected", false) 
+    // })
+    // console.log("hello")
     new Map()
 });
+
+// document.getElementByID.addEventListener()
+
+// d3.on("mouseover", function(d) {
+//     d3.select(this.ele).classed("selected", true)
+// })
+// d3.on("mouseout", function(d) {
+//     d3.select(this.ele).classed("selected", false) 
+// })
 
 
 
